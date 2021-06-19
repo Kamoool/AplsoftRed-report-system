c270e6e4719d16ee912f64e1ec6ff9e7b6a506c9
Kamoool <38004539+Kamoool@users.noreply.github.com>
2021-06-19 10:13:00 +0200

    Initial commit

---
+++ src/test/java/pl/edu/agh/mwo/reporter/BaseTest.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.mwo.reporter;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BaseTest {
+
+    @Test
+    public void baseTest() {
+        assertTrue(true);
+    }
+}
